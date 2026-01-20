03c38c3a8bb689dcf405558191e17306a58091c2
Rosh-coder-stack <roshnikumariofficial8420@gmail.com>
2026-01-20 18:10:05 +0530

    Product service CRUD, validation, search, filter, sort, pagination

--- product-service/src/main/java/com/petshop/product/controller/ProductController.java
+++ product-service/src/main/java/com/petshop/product/controller/ProductController.java
@@ -39,6 +39,57 @@ public class ProductController {
         return productRepository.findByNameContainingIgnoreCase(name);
     }
 
+    @GetMapping("/filter")
+    public List<Product> filterProducts(
+            @RequestParam(required = false) String category,
+            @RequestParam(required = false) Double min,
+            @RequestParam(required = false) Double max) {
+
+        if (category != null && min != null && max != null) {
+            return productRepository
+                    .findByCategoryIgnoreCaseAndPriceBetween(category, min, max);
+        }
+
+        if (category != null) {
+            return productRepository.findByCategoryIgnoreCase(category);
+        }
+
+        if (min != null && max != null) {
+            return productRepository.findByPriceBetween(min, max);
+        }
+        return productRepository.findAll();
+    }
+
+    @GetMapping("/sort")
+    public List<Product> sortProducts(@RequestParam String by) {
+
+        if (by.equalsIgnoreCase("priceAsc")) {
+            return productRepository.findAll(
+                    org.springframework.data.domain.Sort.by("price").ascending()
+            );
+        }
+
+        if (by.equalsIgnoreCase("priceDesc")) {
+            return productRepository.findAll(
+                    org.springframework.data.domain.Sort.by("price").descending()
+            );
+        }
+
+        return productRepository.findAll();
+    }
+
+
+    @GetMapping("/page")
+    public org.springframework.data.domain.Page<Product> getProductsWithPagination(
+            @RequestParam int page,
+            @RequestParam int size) {
+
+        org.springframework.data.domain.Pageable pageable =
+                org.springframework.data.domain.PageRequest.of(page, size);
+
+        return productRepository.findAll(pageable);
+    }
+
     @DeleteMapping("/{id}")
     public String deleteProduct(@PathVariable Long id) {
         productRepository.deleteById(id);
--- product-service/src/main/java/com/petshop/product/repository/ProductRepository.java
+++ product-service/src/main/java/com/petshop/product/repository/ProductRepository.java
@@ -4,5 +4,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 public interface ProductRepository extends JpaRepository< Product, Long> {
     List<Product> findByNameContainingIgnoreCase(String name);
+    List<Product> findByCategoryIgnoreCase(String category);
+
+    List<Product> findByPriceBetween(double min, double max);
+
+    List<Product> findByCategoryIgnoreCaseAndPriceBetween(String category, double min, double max);
+
+
 
 }
